dbd659bedc9f85f9a077780e9f5354d05337e797
Taichen Rose <jatr812@gmail.com>
2025-08-31 21:41:22 -0700

    init: Add swift project

---
+++ JitterClicker/JitterClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JitterClickerApp.swift
+//  JitterClicker
+//
+//  Created by Taichen Rose on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct JitterClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JitterClickerTests/JitterClickerTests.swift
@@ -0,0 +1,16 @@
+//
+//  JitterClickerTests.swift
+//  JitterClickerTests
+//
+//  Created by Taichen Rose on 8/31/25.
+//
+
+import Testing
+
+struct JitterClickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
